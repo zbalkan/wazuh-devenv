26e92df92650fcddda937bdba46f64a4ba75a153
Zafer Balkan <zafer@zaferbalkan.com>
2025-02-08 21:22:15 +0200

    Test organization (#2)
    * Moved regular tests under regression_tests
    
    Signed-off-by: Zafer Balkan <zafer@zaferbalkan.com>
    
    * Cleanup
    
    * Fixed test paths
    
    * Added behavioral test directory
    
    Signed-off-by: Zafer Balkan <zafer@zaferbalkan.com>
    
    * Updated test control parameters
    
    Signed-off-by: Zafer Balkan <zafer@zaferbalkan.com>
    
    ---------
    
    Signed-off-by: Zafer Balkan <zafer@zaferbalkan.com>

--- src/tester.py
+++ src/tester.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-# -*- coding: utf-8 -*-
 
 import argparse
 import logging
@@ -32,20 +31,26 @@ def run_tests(test_directory: str, pattern: str = 'test_*.py', verbosity: int = 
     return test_result
 
 
-def main(enable_builtin: bool, verbosity: int) -> None:
+def main(disable_preflight: bool = False, disable_builtin: bool = False, disable_custom: bool = False, disable_behavioral: bool = False, verbosity: int = 1) -> None:
     # Run preflight tests
-    preflight_result = run_tests('tests.preflight', verbosity=verbosity)
-    if preflight_result and (preflight_result.errors or preflight_result.failures):
-        print('Preflight tests failed. Exiting.')
-        logging.error('Preflight tests failed. Exiting.')
-        exit(1)
+    if disable_preflight is False:
+        preflight_result = run_tests('tests.preflight_tests', verbosity=verbosity)
+        if preflight_result and (preflight_result.errors or preflight_result.failures):
+            print('Preflight tests failed. Exiting.')
+            logging.error('Preflight tests failed. Exiting.')
+            exit(1)
 
     # Conditionally run built-in rule tests if enabled
-    if enable_builtin:
-        run_tests('tests.builtin', verbosity=verbosity)
+    if disable_builtin is False:
+        run_tests('tests.regression_tests.builtin', verbosity=verbosity)
 
     # Run custom rule tests
-    run_tests('tests.custom', verbosity=verbosity)
+    if disable_custom is False:
+        run_tests('tests.regression_tests.custom', verbosity=verbosity)
+
+    # Run behavioral tests
+    if disable_behavioral is False:
+        run_tests('tests.behavioral_tests', verbosity=verbosity)
 
 
 def setup_logging() -> None:
@@ -67,9 +72,19 @@ def parse_arguments() -> argparse.Namespace:
 
     # Add flags and parameters
     parser.add_argument(
-        '--enable-builtin',
+        '--disable-builtin',
         action='store_true',
-        help="Enable running built-in rule tests. Disabled by default."
+        help="Disable running built-in rule tests for regression. Enabled by default."
+    )
+    parser.add_argument(
+        '--disable-custom',
+        action='store_true',
+        help="Disable running custom rule tests for regression. Enabled by default."
+    )
+    parser.add_argument(
+        '--disable-behavioral',
+        action='store_true',
+        help="Disable running behavioral tests. Enabled by default."
     )
     parser.add_argument(
         '--verbosity',
@@ -79,6 +94,13 @@ def parse_arguments() -> argparse.Namespace:
         help="Set verbosity level for test output (0, 1, or 2). Default is 1."
     )
 
+    # Hidden flag to disable preflight tests
+    # Beware that this flag is hidden and should be used with caution
+    parser.add_argument(
+        '--disable-preflight',
+        action='store_true',
+        help=argparse.SUPPRESS
+    )
     # Parse the arguments
     return parser.parse_args()
 
@@ -94,7 +116,11 @@ if __name__ == "__main__":
         args = parse_arguments()
 
         # Run the main function with parsed arguments
-        main(enable_builtin=args.enable_builtin, verbosity=args.verbosity)
+        main(disable_preflight=args.disable_preflight,
+             disable_builtin=args.disable_builtin,
+             disable_custom=args.disable_custom,
+             disable_behavioral=args.disable_behavioral,
+             verbosity=args.verbosity)
 
         logging.info('Exiting.')
     except KeyboardInterrupt:
