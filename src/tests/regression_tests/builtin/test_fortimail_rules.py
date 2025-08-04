f7a70b15bc31493e8907cf6bbc3434519cb8ba94
Zafer Balkan <zafer@zaferbalkan.com>
2025-08-04 17:11:20 +0300

    Removed unused UTF-8 note
    Signed-off-by: Zafer Balkan <zafer@zaferbalkan.com>

--- src/tests/regression_tests/builtin/test_fortimail_rules.py
+++ src/tests/regression_tests/builtin/test_fortimail_rules.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-# -*- coding: utf-8 -*-
+
 
 # These test cases are based on log data and rule descriptions used for regression testing,
 # potentially derived from or inspired by Wazuh rulesets and public log samples.
--- src/tests/regression_tests/custom/test_custom_fortigate_rules.py
+++ src/tests/regression_tests/custom/test_custom_fortigate_rules.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-# -*- coding: utf-8 -*-
+
 import unittest
 
 from internal.logtest import LogtestStatus, send_log
