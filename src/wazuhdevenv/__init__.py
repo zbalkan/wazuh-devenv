f455201c0d4b3631a74068ab7eb02d53b9823998
Zafer Balkan <zafer@zaferbalkan.com>
2026-09-19 00:12:30 +0300

    Add wazuhdevenv package version API

---
+++ src/wazuhdevenv/__init__.py
@@ -0,0 +1,10 @@
+"""wazuh-devenv package."""
+
+from importlib.metadata import PackageNotFoundError, version
+
+try:
+    __version__ = version("wazuh-devenv")
+except PackageNotFoundError:  # source checkout
+    __version__ = "0.4.0.dev0"
+
+__all__ = ["__version__"]
